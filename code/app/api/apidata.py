0e14058b6aaff5bc4d85b7a556497cdca8aadfeb
Matthias Schilling <matthias.schilling@uni-jena.de>
2018-05-27 16:46:24 +0200

    query

--- code/app/api/apidata.py
+++ code/app/api/apidata.py
@@ -1,7 +1,8 @@
 from api import bp
 from elasticsearch import Elasticsearch
-from flask import jsonify
+from flask import jsonify, request
 import datetime
+from collections import defaultdict
 
 es = Elasticsearch('http://localhost:9200')
 
@@ -10,3 +11,44 @@ def get_starttime(starttime):
 	resp = es.search(index='dlrmetadata', doc_type='doc', body={"query": {"match": {"starttime1":
 		            datetime.datetime.strptime(starttime, '%Y-%m-%d %H:%M:%S.%f')}}})
 	return jsonify(resp)
+
+@bp.route('/filter', methods=['GET', 'POST'])
+def filter_data():
+	rang = defaultdict(dict)
+	match = defaultdict(dict)
+	match['mission0'] = ''
+
+	if "mission0" in request.args:
+		match['mission0'] = request.args.get("mission0")
+
+	if "starttimeMin" in request.args:
+		rang["starttime1"]["gte"] = datetime.datetime.strptime(request.args.get("starttimeMin"), '%Y-%m-%d')
+	if "stoptimeMin" in request.args: 
+		rang["stoptime1"]["gte"] = datetime.datetime.strptime(request.args.get("stoptimeMin"), '%Y-%m-%d')
+	if "starttimeMax" in request.args:
+		rang["starttime1"]["lte"] = datetime.datetime.strptime(request.args.get("starttimeMax"), '%Y-%m-%d')
+	if "stoptimeMax" in request.args:
+		rang["stoptime1"]["lte"] = datetime.datetime.strptime(request.args.get("stoptimeMax"), '%Y-%m-%d')
+
+	query = {
+		"must":[
+			{"range":{"stoptime1":rang["stoptime1"]}},
+			{"range":{"starttime1": rang["starttime1"]}}
+		]
+	}
+
+	if match['mission0'] != '':	
+		query["match"] = match
+
+	
+
+	print('mission: ', request.args.get("mission0"))
+	print('q: ', query)
+	print(request.args.get("starttimeMin"))
+
+	resp = es.search(index='dlrmetadata', doc_type='doc', body={"query":{"bool":query}})
+	return jsonify(resp)
+
+@bp.route('/all', methods=['GET'])
+def get_all():
+	pass
--- code/elasticSearch.py
+++ code/elasticSearch.py
@@ -41,7 +41,7 @@ def get_location(row):
 	for _,item in row.iteritems():
 		if type(item)==str and item.startswith("POLYGON(("):
 			#POLYGON((lon lat, lon2 lat2, ...., lonN latN))
-			location = [[float(x) for x in pair.split(" ")[1:]] for pair in item[9:-2].split(",")]
+			location = [[float(x) for x in pair.strip().split(" ")] for pair in item[9:-2].split(",")]
 			return {"type":"polygon", "coordinates":location}
 		elif type(item)==str and item.startswith("POINT("):
 			location = [float(x) for x in item[6:-1].split(" ")]
